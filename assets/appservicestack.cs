727c2459f53185c85589090ae21a7ab8666aeb69
kevinslin <kevinslin@users.noreply.github.com>
2021-03-31 17:49:59 +0000

    deploy: 2bda1cfd871fe1ad822698025aab78989e68a19b

---
+++ assets/appservicestack.cs
@@ -0,0 +1,155 @@
+// Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+
+using Pulumi;
+using Pulumi.AzureNative.Insights;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Sql;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+
+class AppServiceStack : Stack
+{
+    public AppServiceStack()
+    {
+        var resourceGroup = new ResourceGroup("appservice-rg");
+
+        var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Kind = "StorageV2",
+            Sku = new SkuArgs
+            {
+                Name = SkuName.Standard_LRS,
+            },
+        });
+
+        var appServicePlan = new AppServicePlan("asp", new AppServicePlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Kind = "App",
+            Sku = new SkuDescriptionArgs
+            {
+                Tier = "Basic",
+                Name = "B1",
+            },
+        });
+
+        var container = new BlobContainer("zips", new BlobContainerArgs
+        {
+            AccountName = storageAccount.Name,
+            PublicAccess = PublicAccess.None,
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        var blob = new Blob("appservice-blob", new BlobArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            AccountName = storageAccount.Name,
+            ContainerName = container.Name,
+            Type = BlobType.Block,
+            Source = new FileArchive("wwwroot"),
+        });
+
+        var codeBlobUrl = SignedBlobReadUrl(blob, container, storageAccount, resourceGroup);
+        
+        var appInsights = new Component("appInsights", new ComponentArgs
+        {
+            ApplicationType = "web",
+            Kind = "web",
+            ResourceGroupName = resourceGroup.Name,
+        });
+
+        var config = new Config();
+        var username = config.Get("sqlAdmin") ?? "pulumi";
+        var password = config.RequireSecret("sqlPassword");
+        var sqlServer = new Server("sqlserver", new ServerArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            AdministratorLogin = username,
+            AdministratorLoginPassword = password,
+            Version = "12.0",
+        });
+
+        var database = new Database("db", new DatabaseArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            ServerName = sqlServer.Name,
+            Sku = new Pulumi.AzureNative.Sql.Inputs.SkuArgs
+            {
+                Name =  "S0"
+            }
+        });
+
+        var app = new WebApp("app", new WebAppArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            ServerFarmId = appServicePlan.Id,
+            SiteConfig = new SiteConfigArgs
+            {
+                AppSettings = {
+                    new NameValuePairArgs{
+                        Name = "WEBSITE_RUN_FROM_PACKAGE",
+                        Value = codeBlobUrl,
+                    },
+                    new NameValuePairArgs{
+                        Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
+                        Value = appInsights.InstrumentationKey
+                    },
+                    new NameValuePairArgs{
+                        Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
+                        Value = appInsights.InstrumentationKey.Apply(key => $"InstrumentationKey={key}"),
+                    },
+                    new NameValuePairArgs{
+                        Name = "ApplicationInsightsAgent_EXTENSION_VERSION",
+                        Value = "~2",
+                    },
+                },
+                ConnectionStrings = {
+                    new ConnStringInfoArgs
+                    {
+                        Name = "db",
+                        Type = ConnectionStringType.SQLAzure,
+                        ConnectionString = Output.Tuple<string, string, string>(sqlServer.Name, database.Name, password).Apply(t =>
+                        {
+                            (string server, string database, string pwd) = t;
+                            return
+                                $"Server= tcp:{server}.database.windows.net;initial catalog={database};userID={username};password={pwd};Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;";
+                        }),
+                    },
+                },
+            }
+        });
+
+        this.Endpoint = app.DefaultHostName;
+    }
+
+    private static Output<string> SignedBlobReadUrl(Blob blob, BlobContainer container, StorageAccount account, ResourceGroup resourceGroup)
+    {
+        return Output.Tuple<string, string, string, string>(
+            blob.Name, container.Name, account.Name, resourceGroup.Name).Apply(t =>
+        {
+            (string blobName, string containerName, string accountName, string resourceGroupName) = t;
+
+            var blobSAS = ListStorageAccountServiceSAS.InvokeAsync(new ListStorageAccountServiceSASArgs
+            {
+                AccountName = accountName,
+                Protocols = HttpProtocol.Https,
+                SharedAccessStartTime = "2021-01-01",
+                SharedAccessExpiryTime = "2030-01-01",
+                Resource = SignedResource.C,
+                ResourceGroupName = resourceGroupName,
+                Permissions = Permissions.R,
+                CanonicalizedResource = "/blob/" + accountName + "/" + containerName,
+                ContentType = "application/json",
+                CacheControl = "max-age=5",
+                ContentDisposition = "inline",
+                ContentEncoding = "deflate",
+            });
+            return Output.Format($"https://{accountName}.blob.core.windows.net/{containerName}/{blobName}?{blobSAS.Result.ServiceSasToken}");
+        });
+    }
+
+    [Output] public Output<string> Endpoint { get; set; }
+}
---
+++ assets/azure_test.go
@@ -0,0 +1,287 @@
+// +build Azure all
+
+package test
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccAzureCsAppService(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-cs-appservice"),
+			Config: map[string]string{
+				"sqlPassword": "2@Password@2",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["Endpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Greetings from Azure App Service")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureCsWebserver(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-cs-webserver"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPResult(t, stack.Outputs["IpAddress"].(string), nil, func(body string) bool {
+					return assert.Contains(t, body, "Hello, World")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureFsAppService(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-fs-appservice"),
+			Config: map[string]string{
+				"sqlPassword": "2@Password@2",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["endpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Greetings from Azure App Service")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureGoAci(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-go-aci"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["endpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Hello, containers!")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureGoWebserverComponent(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-go-webserver-component"),
+			Config: map[string]string{
+				"username": "webmaster",
+				"password": "Password1234!",
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzurePyAppService(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-py-appservice"),
+			Config: map[string]string{
+				"sqlPassword": "2@Password@2",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["endpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Greetings from Azure App Service")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzurePyAppServiceDocker(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-py-appservice-docker"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["helloEndpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Hello, world!")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzurePyArmTemplate(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-py-arm-template"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzurePyVmScaleSet(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-py-vm-scaleset"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPResult(t, stack.Outputs["public_address"].(string), nil, func(body string) bool {
+					return assert.Contains(t, body, "nginx")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzurePyWebserver(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-py-webserver"),
+			Config: map[string]string{
+				"username": "testuser",
+				"password": "testTEST1234+-*/",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPHelloWorld(t, stack.Outputs["public_ip"], nil)
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsAppService(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-ts-appservice"),
+			Config: map[string]string{
+				"sqlPassword": "2@Password@2",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["endpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Greetings from Azure App Service")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsAppServiceDocker(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-ts-appservice-docker"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertAppServiceResult(t, stack.Outputs["getStartedEndpoint"], func(body string) bool {
+					return assert.Contains(t, body, "Azure App Service")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsArmTemplate(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-ts-arm-template"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsFunctions(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-ts-functions"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPResult(t, stack.Outputs["endpoint"], nil, func(body string) bool {
+					return assert.Contains(t, body, "Hello from Node.js, Pulumi")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsStreamAnalytics(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-ts-stream-analytics"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsVmScaleset(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "classic-azure-ts-vm-scaleset"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPResult(t, stack.Outputs["publicAddress"].(string), nil, func(body string) bool {
+					return assert.Contains(t, body, "nginx")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func TestAccAzureTsWebserver(t *testing.T) {
+	test := getAzureBase(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "..", "..", "azure-ts-webserver"),
+			Config: map[string]string{
+				"username": "webmaster",
+				"password": "MySuperS3cretPassw0rd",
+			},
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				assertHTTPResult(t, stack.Outputs["ipAddress"].(string), nil, func(body string) bool {
+					return assert.Contains(t, body, "Hello, World")
+				})
+			},
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getAzureEnvironment() string {
+	azureEnviron := os.Getenv("ARM_ENVIRONMENT")
+	if azureEnviron == "" {
+		azureEnviron = "public"
+		fmt.Println("Defaulting ARM_ENVIRONMENT to 'public'.  You can override using the ARM_ENVIRONMENT variable")
+	}
+
+	return azureEnviron
+}
+
+func getAzureLocation() string {
+	azureLocation := os.Getenv("ARM_LOCATION")
+	if azureLocation == "" {
+		azureLocation = "westus"
+		fmt.Println("Defaulting ARM_LOCATION to 'westus'.  You can override using the ARM_LOCATION variable")
+	}
+
+	return azureLocation
+}
+
+func getAzureBase(t *testing.T) integration.ProgramTestOptions {
+	azureEnviron := getAzureEnvironment()
+	azureLocation := getAzureLocation()
+	base := getBaseOptions(t)
+	azureBase := base.With(integration.ProgramTestOptions{
+		Config: map[string]string{
+			"azure:environment":     azureEnviron,
+			"azure:location":        azureLocation,
+			"azure-native:location": azureLocation,
+		},
+	})
+	return azureBase
+}
